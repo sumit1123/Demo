cbe95107aacca4594084c8b7538b047766b6190c
sumitgarg <smtgarg3@gmail.com>
2023-05-13 12:57:08 +0530

    sssscdd

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        print("Hello world")
+        print("Hello worldsdsdsd")
     }
 }
